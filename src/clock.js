6555245032db5da2986d7f3b1d91493bfb566c32
NoMoreBuild <rainha1005@gmail.com>
2020-06-13 16:10:13 +0900

    add asking name

--- src/clock.js
+++ src/clock.js
@@ -1,22 +1,25 @@
 //html 태그 불러오기
-const clock = document.querySelector(".clock");
+const clock24 = document.querySelector(".clock24");
+
+//css 스타일
+const show = "show";
 
 //현재시간 받아오기
-function getDate() {
+function getDate24() {
   const hour = new Date().getHours();
   const minute = new Date().getMinutes();
   const second = new Date().getSeconds();
 
-  clock.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${
+  clock24.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${
     minute < 10 ? `0${minute}` : minute
   } : ${second < 10 ? `0${second}` : second}`;
 }
 
 //기본동작
 function init() {
-  //초당 시간 갱신
+  getDate24();
   setInterval(() => {
-    getDate();
+    getDate24();
   }, 1000);
 }
 
--- src/greeting.js
+++ src/greeting.js
@@ -1,18 +1,46 @@
-const USER_LS = "Username";
+//인풋 태그
+const userInput = document.querySelector(".userInput"),
+  form = document.querySelector(".js-form"),
+  info = document.querySelector(".js-userInfo");
 
-function saveLS(obj) {
-  localStorage.setItem(USER_LS, obj);
+//유저 로컬 스토리지 이름
+const USER_LS = "Username",
+  SHOWING_CN = "showing";
+
+//로컬 스토리지 저장
+function saveLS(value) {
+  localStorage.setItem(USER_LS, value);
 }
 
+function submitInput(e) {
+  e.preventDefault();
+  const currValue = userInput.value;
+  paintName(currValue);
+  saveLS(currValue);
+}
+
+function askName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", submitInput);
+}
+
+function paintName(value) {
+  form.classList.remove(SHOWING_CN);
+  info.classList.add(SHOWING_CN);
+  info.innerHTML = `welcome ${value}`;
+}
+
+//로컬 스토리지 존재 여부 확인
 function getLS() {
   const getUserLS = localStorage.getItem(USER_LS);
   if (getUserLS === null) {
-    console.log("its null");
+    askName();
   } else {
-    console.log("not null");
+    paintName(getUserLS);
   }
 }
 
+//기본 동작
 function init() {
   getLS();
 }
